74132ceb967e1425f7c3c2599d60141c6cb0fd46
Büşra ÇİÇEK <37375743+bussracicekk@users.noreply.github.com>
2020-09-21 16:30:48 +0300

    Add files via upload

---
+++ WorkShopProject/WorkshopProject.Designer.cs
@@ -0,0 +1,262 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class WorkshopProject
+    {
+        private DataGrid myDataGrid;
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myDataGrid = new System.Windows.Forms.DataGrid();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView6 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.myDataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myDataGrid
+            // 
+            this.myDataGrid.DataMember = "";
+            this.myDataGrid.HeaderForeColor = System.Drawing.SystemColors.ControlText;
+            this.myDataGrid.Location = new System.Drawing.Point(0, 0);
+            this.myDataGrid.Name = "myDataGrid";
+            this.myDataGrid.Size = new System.Drawing.Size(130, 80);
+            this.myDataGrid.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(609, 272);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView2.Location = new System.Drawing.Point(715, 32);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(606, 272);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView3.Location = new System.Drawing.Point(16, 376);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(606, 121);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView4.Location = new System.Drawing.Point(715, 376);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.RowHeadersWidth = 51;
+            this.dataGridView4.RowTemplate.Height = 24;
+            this.dataGridView4.Size = new System.Drawing.Size(606, 121);
+            this.dataGridView4.TabIndex = 3;
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView5.Location = new System.Drawing.Point(268, 540);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.RowHeadersWidth = 51;
+            this.dataGridView5.RowTemplate.Height = 24;
+            this.dataGridView5.Size = new System.Drawing.Size(775, 121);
+            this.dataGridView5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(100, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Most returning 5 products";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(868, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Most sold 5 products";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(70, 344);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(314, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Total costs of sales in the last week";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(811, 344);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(361, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Total costs of re-turnings in the last week";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(437, 517);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(394, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Most employee (user) with re-turned products";
+            // 
+            // dataGridView6
+            // 
+            this.dataGridView6.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView6.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView6.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView6.Location = new System.Drawing.Point(268, 710);
+            this.dataGridView6.Name = "dataGridView6";
+            this.dataGridView6.RowHeadersWidth = 51;
+            this.dataGridView6.RowTemplate.Height = 24;
+            this.dataGridView6.Size = new System.Drawing.Size(775, 121);
+            this.dataGridView6.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(441, 687);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(378, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Most role with re-turned products and count";
+            // 
+            // WorkshopProject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1356, 856);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView5);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "WorkshopProject";
+            this.Text = "Workshop Report";
+            ((System.ComponentModel.ISupportInitialize)(this.myDataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private DataGridView dataGridView3;
+        private DataGridView dataGridView4;
+        private DataGridView dataGridView5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private DataGridView dataGridView6;
+        private Label label6;
+    }
+}
+
---
+++ WorkShopProject/WorkshopProject.cs
@@ -0,0 +1,93 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class WorkshopProject : Form
+    {
+        public WorkshopProject()
+        {
+            InitializeComponent();
+            Workshop();
+        }
+
+        private void Workshop()
+        {
+            NpgsqlConnection connection = new NpgsqlConnection("Server=localhost; Port=5432; Database=workshopProject; User Id = postgres; Password=busra123;");
+            DataSet dataset = new DataSet();
+            DataSet dataset1 = new DataSet();
+            DataSet dataset2 = new DataSet();
+            DataSet dataset3 = new DataSet();
+            DataSet dataset4 = new DataSet();
+            DataSet dataset5 = new DataSet();
+            try
+            {
+                connection.Open();
+            }
+            catch(Exception ex) 
+            {
+                throw;
+            }
+            string returning = "SELECT p.name as ProductNames, count(s.IdProduct) as productcount FROM public.\"product\" as p right join public.\"saleproductuser\" as s " +
+                "on s.IdProduct = p.Id where p.cost < 0 group by p.name order by count(s.IdProduct) desc limit 5";// Top 5 products that most returning
+            NpgsqlDataAdapter addReturn = new NpgsqlDataAdapter(returning, connection);
+            addReturn.Fill(dataset);
+
+            dataGridView1.DataSource = dataset.Tables[0];
+         
+            string sold = "SELECT p.name as ProductNames,count(s.IdProduct) as productcount FROM public.\"product\" as p right join public.\"saleproductuser\" as s " +
+                "on s.IdProduct = p.Id where p.cost > 0 group by p.name order by count(s.IdProduct) desc limit 5 ";// Top 5 products that most sold
+            NpgsqlDataAdapter addSold = new NpgsqlDataAdapter(sold, connection);
+            addSold.Fill(dataset1);
+            dataGridView2.DataSource = dataset1.Tables[0];
+
+
+            string totalcostofsold = " Select sum(totalcost) as Totalprice from public.\"sale\" where Date between timestamp '2020-03-30 00:00:00'::date - extract(dow from timestamp '2020-03-30 00:00:00')::integer - 7" +
+                "and timestamp '2020-03-30 00:00:00'::date - extract(DOW from timestamp '2020-03-30 00:00:00')::integer";//Total Cost Of sold in last week
+
+            NpgsqlDataAdapter addtotalcostofsold = new NpgsqlDataAdapter(totalcostofsold, connection);
+            addtotalcostofsold.Fill(dataset2);
+            dataGridView3.DataSource = dataset2.Tables[0];
+
+            string totalcostofreturning = " Select sum(p.cost) as Totalprice from public.\"saleproductuser\" as s left join public.\"product\" as p " +
+                "on p.Id = s.IdProduct left join public.\"sale\" as sa on sa.Id = s.IdSale " +
+                "where p.cost<0 and Date between timestamp '2020-03-30 00:00:00'::date - extract(dow from timestamp '2020-03-30 00:00:00')::integer - 7 " +
+                "and timestamp '2020-03-30 00:00:00'::date - extract(DOW from timestamp '2020-03-30 00:00:00')::integer";//Total Cost Of Returning in Last Week
+
+            NpgsqlDataAdapter addtotalcostofreturning = new NpgsqlDataAdapter(totalcostofreturning, connection);
+            addtotalcostofreturning.Fill(dataset3);
+            dataGridView4.DataSource = dataset3.Tables[0];
+
+            string mostemployee = "select concat(u.name,' ',u.lastname) as Employee ,r.Name as RoleName from public.\"users\" as u right join public.\"saleproductuser\" as s " +
+                "on s.IdUser = u.IdUser left join public.\"product\" as p on p.Id = s.IdProduct left join public.\"userrole\" as ur " +
+                "on ur.UserId = u.IdUser left join public.\"role\" as r " +
+                "on r.Id = ur.RoleId where p.cost < 0 group by u.IdUser,r.Name order by count(u.IdUser) desc limit 3; ";//Most Employee with re-turned products
+
+            NpgsqlDataAdapter addmostemployee = new NpgsqlDataAdapter(mostemployee, connection);
+            addmostemployee.Fill(dataset4);
+            dataGridView5.DataSource = dataset4.Tables[0];
+
+            string mostrole = "select r.Name as RoleName,count(r.name) as rolecount from public.\"users\" as u right join public.\"saleproductuser\" as s " +
+                "on s.IdUser = u.IdUser left join public.\"product\" as p on p.Id = s.IdProduct left join public.\"userrole\" as ur " +
+                "on ur.UserId = u.IdUser left join public.\"role\" as r " +
+                "on r.Id = ur.RoleId where p.cost < 0 group by r.Id,r.Name order by count(r.Id) desc limit 3; ";
+
+            NpgsqlDataAdapter addmostrole = new NpgsqlDataAdapter(mostrole, connection);
+            addmostrole.Fill(dataset5);
+            dataGridView6.DataSource = dataset5.Tables[0];
+            connection.Close();
+
+        }
+
+      
+    }
+}
